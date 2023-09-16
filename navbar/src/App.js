e65f5ff95045acaf5e658a3a7d4b349c2ee4fbca
jayala5 <105734297+jayala5@users.noreply.github.com>
2023-09-16 20:28:55 -0300

    Nuevos cambios 16/9 Julian

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import Items from './Components/Items';
 
+//Ver donde poner ITEMS.jsx 
+//No aparece HMENU 
 function App() {
   return (
     <React.Fragment>
-      <Navbar/>
+      <Navbar></Navbar>
     </React.Fragment>
   );
 }
